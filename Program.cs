fd563918e5e80d3c98e50401cb3ee1a42da000c7
Yazdan <Yazdan@DESKTOP-D868KSI>
2025-08-11 13:14:13 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2, result = 0;
+        char operation;
+
+        //Console.WriteLine("ماشین حساب ساده به زبان C#");
+        Console.Write("number one: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write(" (+ - * /): ");
+        operation = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("number two: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.\r\n!");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Error.Error");
+                return;
+        }
+
+        Console.WriteLine($"the result: {result}");
+        
+    }
+}
+
